94b5b1cfdea33b310df7ab578096a4f0543ba288
河边骨 <navylee0210@163.com>
2020-07-09 15:15:02 +0800

    1111

--- Source/Application/HeBianGu.App.Demo.ImagePlayer/MainWindow.xaml.cs
+++ Source/Application/HeBianGu.App.Demo.ImagePlayer/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace HeBianGu.App.Demo.ImagePlayer
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
